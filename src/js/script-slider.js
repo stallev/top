a1e08307fdb87244867f098ca5359567a1bcc6bc
Alexander <stwork87@gmail.com>
2020-09-16 16:43:12 +0300

    changed photos

---
+++ src/js/script-slider.js
@@ -0,0 +1,18 @@
+console.log(125);
+$(document).ready(function(){
+  $('.sliders').slick({
+   /*arrows: true,
+    dots: true,
+    //adaptiveHeight: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 300,
+    easing: 'ease',
+    infinite: true,
+    initialSlide: 0,
+    autoplay: false,
+    autoplaySpeed: 1500,
+    pauseOnFocus: true,
+    pauseOnHover: true,*/
+  });
+});
